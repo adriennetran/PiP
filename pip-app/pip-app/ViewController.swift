081ddd8ec6c048160deee3b9cdb65e13e4072c64
AngelInteractive <slattery.peter.j@gmail.com>
2015-04-05 18:44:11 -0400

    ScrollView: ViewController is now a scrollViewDelegate. Added scroll and zoom functionality to viewController.

--- pip-app/pip-app/ViewController.swift
+++ pip-app/pip-app/ViewController.swift
@@ -8,30 +8,85 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
+	
+	
+	@IBOutlet var scrollView: UIScrollView!
 	
 	//Array of Pips which are currently in the workspace
 	var activePips: [BasePipView] = []
+	var containerView: UIView!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		
-		var testColorPip = ColorPipView(point: CGPointMake(25, 25))
-		var testTextPip = TextPipView(point: CGPointMake(200, 100))
+		containerView = UIView(frame: CGRectMake(0, 0, 1440, 1440))
+		scrollView.addSubview(containerView)
 		
-		self.view.addSubview(testColorPip)
-		self.view.addSubview(testTextPip)
+		var backgroundView = UIView(frame: CGRectMake(0, 0, 1440, 1440))
+		backgroundView.backgroundColor = UIColor.whiteColor()
+		containerView.addSubview(backgroundView)
+		
+		scrollView.contentSize = containerView.bounds.size
+		
+		var testColorPip = ColorPipView(point: CGPointMake(25, 25))
+		var testTextPip = TextPipView(point: CGPointMake(100, 100))
+		
+		containerView.addSubview(testColorPip)
+		containerView.addSubview(testTextPip)
 		
 		activePips.append(testColorPip)
 		activePips.append(testTextPip)
 		
 		self.view.userInteractionEnabled = true;
+		scrollView.userInteractionEnabled = true;
+		
+		//Sets up a gestureRecognizer that lets a double tap zoom in
+		//
+		var doubleTapRecognizer = UITapGestureRecognizer(target: self, action: "scrollViewDoubleTapped:")
+		doubleTapRecognizer.numberOfTapsRequired = 2
+		doubleTapRecognizer.numberOfTouchesRequired = 1
+		scrollView.addGestureRecognizer(doubleTapRecognizer)
+		
+		let scrollViewFrame: CGRect = scrollView.frame
+		let scaleWidth = scrollViewFrame.size.width / scrollView.contentSize.width
+		let scaleHeight = scrollViewFrame.size.height / scrollView.contentSize.height
+		let minScale: CGFloat = min(scaleWidth, scaleHeight)
+		
+		scrollView.minimumZoomScale = minScale
+		scrollView.maximumZoomScale = 1.0
+		scrollView.zoomScale = minScale
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
+	}
+	
+	func scrollViewDoubleTapped(recognizer: UITapGestureRecognizer) {
+		println("1")
+		let pointInView = recognizer.locationInView(containerView)
+		
+		var newZoomScale = min((scrollView.zoomScale * 1.5), scrollView.maximumZoomScale)
+		
+		let scrollViewSize = scrollView.bounds.size
+		let w = scrollViewSize.width / newZoomScale
+		let h = scrollViewSize.height / newZoomScale
+		let x = pointInView.x - (w / 2.0)
+		let y = pointInView.y - (h / 2.0)
+		
+		let rectToZoomTo = CGRectMake(x, y, w, h)
+		
+		scrollView.zoomToRect(rectToZoomTo, animated: true)
+	}
+	
+	func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+		return containerView
+	}
+	
+	func scrollViewDidZoom(scrollView: UIScrollView) {
+		return
 	}
 }
 
