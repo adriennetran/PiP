7f5e08fcef5f949bf53f8b791e6dc5ad88f2996e
AngelInteractive <slattery.peter.j@gmail.com>
2015-04-05 10:39:42 -0400

    BasePip.swift and TextPip.swift created

---
+++ pip-app/pip-app/BasePip.swift
@@ -0,0 +1,16 @@
+//
+//  BasePip.swift
+//  pip-app
+//
+//  Created by Peter Slattery on 4/5/15.
+//  Copyright (c) 2015 Peter Slattery. All rights reserved.
+//
+
+import Foundation
+
+class BasePip {
+	
+	init(){
+		
+	}
+}
---
+++ pip-app/pip-app/TextPip.swift
@@ -0,0 +1,16 @@
+//
+//  TextPip.swift
+//  pip-app
+//
+//  Created by Peter Slattery on 4/5/15.
+//  Copyright (c) 2015 Peter Slattery. All rights reserved.
+//
+
+import Foundation
+
+class TextPip: BasePip {
+	
+	override init(){
+		super.init()
+	}
+}
