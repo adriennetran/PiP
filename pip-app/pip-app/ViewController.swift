0484850485ace5da5832d2d8cb3a44b3fe901018
adrienne <adrienne@adriennetran.com>
2015-04-23 02:23:12 -0400

    camera

--- pip-app/pip-app/ViewController.swift
+++ pip-app/pip-app/ViewController.swift
@@ -21,34 +21,55 @@ class ViewController: UIViewController, UIScrollViewDelegate, UINavigationContro
 	var staticScreenElements: [(view: UIView, pos: CGPoint)] = []
 
     
-    // camera stuff
+    
+    /* We will use this variable to determine if the viewDidAppear:
+    method of our view controller is already called or not. If not, we will
+    display the camera view */
     var beenHereBefore = false
     var controller: UIImagePickerController?
     
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
-        println("Picker returned successfully")
-        
-        let mediaType:AnyObject? = info[UIImagePickerControllerMediaType]
-        if let type:AnyObject = mediaType{
+    func imagePickerController(picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [NSObject : AnyObject]){
             
-            if type is String{
-                let stringType = type as! String
+            println("Picker returned successfully")
+            
+            let mediaType:AnyObject? = info[UIImagePickerControllerMediaType]
+            
+            if let type:AnyObject = mediaType{
                 
-                if stringType == kUTTypeImage as NSString as NSString {
-                    // get meta data
-                    let metadata = info[UIImagePickerControllerMediaMetadata] as? NSDictionary
-                    if let theMetaData = metadata {
-                        let image = info[UIImagePickerControllerOriginalImage] as? UIImage
-                        if let theImage = image{
-                            println ("Image meta data= \(theMetaData)")
-                            println("Image = \(theImage)")
+                if type is String{
+                    let stringType = type as! String
+                    
+//                    if stringType == kUTTypeMovie as! String{
+//                        let urlOfVideo = info[UIImagePickerControllerMediaURL] as? NSURL
+//                        if let url = urlOfVideo{
+//                            println("Video URL = \(url)")
+//                        }
+//                    }
+                    
+                    if stringType == kUTTypeImage as! String{
+                        /* Let's get the metadata. This is only for images. Not videos */
+                        let metadata = info[UIImagePickerControllerMediaMetadata]
+                            as? NSDictionary
+                        if let theMetaData = metadata{
+                            let image = info[UIImagePickerControllerOriginalImage]
+                                as? UIImage
+                            if let theImage = image{
+                                println("Image Metadata = \(theMetaData)")
+                                println("Image = \(theImage)")
+                            }
                         }
                     }
+                    
                 }
-                picker.dismissViewControllerAnimated(true, completion: nil)
+            }
             
+            picker.dismissViewControllerAnimated(true, completion: nil)
     }
-        }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        println("Picker was cancelled")
+        picker.dismissViewControllerAnimated(true, completion: nil)
     }
     
     func isCameraAvailable() -> Bool{
@@ -57,43 +78,58 @@ class ViewController: UIViewController, UIScrollViewDelegate, UINavigationContro
     
     func cameraSupportsMedia(mediaType: String,
         sourceType: UIImagePickerControllerSourceType) -> Bool{
-        
-        let availableMediaTypes = UIImagePickerController.availableMediaTypesForSourceType(sourceType) as! [String]
-        for type in availableMediaTypes{
-            if type == mediaType{
-                return true
+            
+            let availableMediaTypes =
+            UIImagePickerController.availableMediaTypesForSourceType(sourceType) as!
+                [String]?
+            
+            if let types = availableMediaTypes{
+                for type in types{
+                    if type == mediaType{
+                        return true
+                    }
+                }
             }
-        }
-        return false
+            
+            return false
     }
-    
     
     func doesCameraSupportTakingPhotos() -> Bool{
-        return cameraSupportsMedia(kUTTypeImage as NSString as String, sourceType: .Camera)
+        return cameraSupportsMedia(kUTTypeImage as! String, sourceType: .Camera)
     }
-
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+        
         if beenHereBefore{
-    /* Only display the picker once as the viewDidAppear: method gets called whenever the view of our view controller is displayed */
-        return;
-    } else{
-        beenHereBefore = true
-    }
-    if isCameraAvailable() && doesCameraSupportTakingPhotos(){
-        controller = UIImagePickerController()
+            /* Only display the picker once as the viewDidAppear: method gets
+            called whenever the view of our view controller gets displayed */
+            return;
+        } else {
+            beenHereBefore = true
+        }
+        
+        if isCameraAvailable() && doesCameraSupportTakingPhotos(){
+            
+            controller = UIImagePickerController()
+            
             if let theController = controller{
                 theController.sourceType = .Camera
+                
+                theController.mediaTypes = [kUTTypeImage as! String]
+                
                 theController.allowsEditing = true
-                theController.allowsEditing = true
+                theController.delegate = self
+                
                 presentViewController(theController, animated: true, completion: nil)
-                presentViewController(theController, animated: true, completion: nil)
+            }
+            
+        } else {
+            println("Camera is not available")
         }
-    } else{
-        println("Camera is not available")
+        
     }
-    }
+    
 
 
 	var trashCanButton: UIView!
