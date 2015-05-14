514886f53078ffd98c166dbf95da9099ee5203b3
AngelInteractive <slattery.peter.j@gmail.com>
2015-05-14 05:01:39 -0400

    audio updated

--- pip-app/pip-app/AudioControllerDelegate.swift
+++ pip-app/pip-app/AudioControllerDelegate.swift
@@ -12,34 +12,61 @@ import AVFoundation
 
 var _audioController: AudioControllerDelegate = AudioControllerDelegate()
 
-enum AudioClips: String {
-	case Delete = "PipSamples/01_deletionSounds/char01A"
-	case Handshake = "PipSamples/02_handshakeSounds/char01A"
-	case Move = "PipSamples/02_handshakeSounds/char02C"
+enum AudioClips: Int {
+	case Delete
+	case Handshake
+	case Move
 }
 
 class AudioControllerDelegate {
 	
 	var clips: [AudioClips : AVAudioPlayer] = [:]
 	
+	var clipPaths: [AudioClips: [String]] = [:]
+	
 	init() {
+		
+		clipPaths[.Delete] = ["PipSamples/01_deletionSounds/0", "PipSamples/01_deletionSounds/1", "PipSamples/01_deletionSounds/2"]
+		clipPaths[.Handshake] = ["PipSamples/02_handshakeSounds/0", "PipSamples/02_handshakeSounds/1", "PipSamples/02_handshakeSounds/2"]
+		clipPaths[.Move] = ["PipSamples/03_moveSounds/0", "PipSamples/03_moveSounds/1", "PipSamples/03_moveSounds/2"]
 		
 	}
 	
 	func playSound(clip: AudioClips){
 		
-		if clips[clip] != nil {
-			let clipToPlay = clips[clip]
-			clipToPlay!.play()
+		if let clip: AVAudioPlayer = clips[clip] {
+			clip.play()
 		}else{
-		
 			var soundClip = AVAudioPlayer()
-			soundClip = self.setupAudioPlayerWithFile(clip.rawValue, type: "wav")
+			soundClip = self.setupAudioPlayerWithFile(clipPaths[clip]![0], type: "wav")
 			clips[clip] = soundClip
 			soundClip.prepareToPlay()
-			
 			soundClip.play()
 		}
+		
+		// TO DO: Make Audio Random
+//		if let paths = clipPaths[clip]{
+//			if paths.count > 0 {
+//				let randIndex: Int = Int(arc4random_uniform(UInt32(paths.count)))
+//			
+//				if let clipArray: [AVAudioPlayer] = clips[clip]{
+//					let numClips: Int = clipArray.count
+//					let clipAt: AVAudioPlayer? = clipArray[randIndex]
+//					
+//					if numClips > randIndex && clipAt != nil {
+//						let clipToPlay = clips[clip]?[randIndex]
+//						clipToPlay!.play()
+//					}
+//				}else{
+//					var soundClip = AVAudioPlayer()
+//					soundClip = self.setupAudioPlayerWithFile(paths[randIndex], type: "wav")
+//					clips[clip]?.insert(soundClip, atIndex: randIndex)
+//					
+//					soundClip.prepareToPlay()
+//					soundClip.play()
+//				}
+//			}
+//		}
 	}
 	
 	func setupAudioPlayerWithFile(file: String, type: String) -> AVAudioPlayer {
